90bde9b3a35c45386929d7c098b984f99945a912
pkaczor <newion@o2.pl>
2015-05-09 11:50:08 +0200

    allow first request in e2e test to be repeated

--- e2e-tests/src/main/scala/ecommerce/tests/e2e/EcommerceSystemTestDriver.scala
+++ e2e-tests/src/main/scala/ecommerce/tests/e2e/EcommerceSystemTestDriver.scala
@@ -1,9 +1,10 @@
 package ecommerce.tests.e2e
 
 import org.iainhull.resttest.driver.Jersey
+import org.scalatest.BeforeAndAfterAll
 import pl.newicom.dddd.utils.UUIDSupport
 
-trait EcommerceSystemTestDriver extends TestDriver with Jersey with UUIDSupport {
+trait EcommerceSystemTestDriver extends TestDriver with Jersey with UUIDSupport with BeforeAndAfterAll {
 
   val defBuilder = RequestBuilder.emptyBuilder.addHeaders(
     ("Content-Type", "application/json")
--- e2e-tests/src/test/scala/ecommerce/tests/e2e/SystemSpec.scala
+++ e2e-tests/src/test/scala/ecommerce/tests/e2e/SystemSpec.scala
@@ -1,10 +1,12 @@
 package ecommerce.tests.e2e
 
+import ecommerce.sales.{CreateReservation, salesOffice}
+import ecommerce.tests.e2e.SystemSpec._
 import org.iainhull.resttest.Api.Status.OK
 import org.json4s.ext.{JodaTimeSerializers, UUIDSerializer}
 import org.json4s.{DefaultFormats, Formats}
-import ecommerce.sales.{CreateReservation, salesOffice}
-import SystemSpec._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Seconds, Span}
 
 object SystemSpec {
 
@@ -13,10 +15,14 @@ object SystemSpec {
   val sales_read = sales.copy(port = 9110)
 
   implicit val formats: Formats = salesOffice.serializationHints ++ DefaultFormats ++ JodaTimeSerializers.all + UUIDSerializer
-
 }
 
-class SystemSpec extends EcommerceSystemTestDriver {
+class SystemSpec extends EcommerceSystemTestDriver with Eventually {
+
+  implicit override val patienceConfig = PatienceConfig(
+    timeout = scaled(Span(10, Seconds)),
+    interval = scaled(Span(2, Seconds))
+  )
 
   "Ecommerce system" should {
 
@@ -25,7 +31,9 @@ class SystemSpec extends EcommerceSystemTestDriver {
 
     using(sales_write) { implicit b =>
       "create reservation" in {
-        POST command CreateReservation(reservationId, customerId) should have (StatusCode(OK))
+        eventually {
+          POST command CreateReservation(reservationId, customerId) should have(StatusCode(OK))
+        }
       }
     }
 
