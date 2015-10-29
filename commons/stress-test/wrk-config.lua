b6d1eaab20f712157160180ad12358bbeafc645d
pkaczor <newion@o2.pl>
2015-10-29 23:01:28 +0100

    improve uuid creation

--- commons/stress-test/wrk-config.lua
+++ commons/stress-test/wrk-config.lua
@@ -2,26 +2,16 @@ local json = require("lib/json")
 local data = require("lib/data")
 local utils = require("lib/utils")
 local socket = require("socket")
-local uuid = require("uuid")
 
-local threadsCounter = 0
+local threadGlobalCounter = 0
 
 local function log(msg)
     print(msg)
 end
 
-local function newUuid()
-    math.randomseed(socket.gettime())
-    uuid.randomseed(math.random()*100000000000)
-    return uuid()
-end
---
--- wrk functions
---
-
 function setup(thread)
-    threadsCounter = threadsCounter + 1
-    thread:set("customerId", threadsCounter)
+    threadGlobalCounter = threadGlobalCounter + 1
+    thread:set("threadCounter", threadGlobalCounter)
 end
 
 function init(args)
@@ -30,7 +20,11 @@ function init(args)
     requestTemplates = json.loadFile(dir .. "/requests.json")
     sessionCounter = 1
     step = 1
-    reservationId = newUuid()
+    uuid = require("uuid")
+    math.randomseed(socket.gettime())
+    uuid.randomseed(math.random()*100000000000)
+
+    reservationId = uuid()
 end
 
 function delay()
@@ -41,24 +35,24 @@ function request()
     req = data.request {
         template      = requestTemplates[step],
         reservationId = reservationId,
-        customerId    = "customer-" .. customerId
+        customerId    = "customer-" .. threadCounter
     }
 
     wrk.port = req.port
     wrk.port = req.port
+    print(reservationId)
     return wrk.format(req.method, req.path, req.headers, req.bodyStr)
 end
 
 function response(status, headers, body)
-    utils.logResponse(status, headers, body, step, customerId)
+    utils.logResponse(status, headers, body, step, threadCounter)
     if step == 6 then
         sessionCounter = sessionCounter + 1
         if sessionCounter > maxSessions then
-            --log(string.format("Stopping customer: %d", customerId))
             wrk.thread:stop()
         else
             step = 1
-            reservationId = newUuid()
+            reservationId = uuid()
         end
     else
         step = step + 1
