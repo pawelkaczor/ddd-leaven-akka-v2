084bc9f9ca0881372114761898c69d64add02fd7
pkaczor <newion@o2.pl>
2015-10-30 16:59:08 +0100

    test results from "reactive-in-memory" version

--- commons/stress-test/stress.sh
+++ commons/stress-test/stress.sh
@@ -4,5 +4,5 @@ configDir=/mnt/stress-test
 
 cd $configDir
 
-# 24 threads, 30 seconds
-wrk -t24 -c24 -d30s -s $configDir/wrk-config.lua http://127.0.0.1:80 -- $configDir
+# 50 threads, 30 seconds
+wrk -t50 -c50 -d30s -s $configDir/wrk-config.lua http://127.0.0.1:80 -- $configDir
