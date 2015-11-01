caf97f0834392b13682a5e3bda3a4e6d17744fff
pkaczor <newion@o2.pl>
2015-11-01 15:50:09 +0100

    switch to Eventstore 3.3.0

--- commons/stress-test/stress.sh
+++ commons/stress-test/stress.sh
@@ -4,5 +4,7 @@ configDir=/mnt/stress-test
 
 cd $configDir
 
-# 50 threads, 30 seconds
-wrk -t50 -c50 -d30s -s $configDir/wrk-config.lua http://127.0.0.1:80 -- $configDir
+threadTotal=50
+testDuration=30s
+
+wrk -t${threadTotal} -c${threadTotal} -d${testDuration} -s $configDir/wrk-config.lua http://127.0.0.1:80 -- $configDir
