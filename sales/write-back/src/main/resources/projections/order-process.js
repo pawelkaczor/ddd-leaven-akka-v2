987acd1f8196dd270c1136eab0d7049d25452175
pkaczor <newion@o2.pl>
2015-11-10 21:11:33 +0100

    update projections

--- sales/write-back/src/main/resources/projections/order-process.js
+++ sales/write-back/src/main/resources/projections/order-process.js
@@ -3,21 +3,21 @@ fromStreams(['$ce-Reservation', '$ce-Invoice']).
         'ecommerce.sales.ReservationConfirmed' : function(s,e) {
             var metadata = JSON.parse(e.metadataRaw);
             var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.metadata.correlationId) {
+            if (body.payload && !metadata.content.correlationId) {
                 linkTo('sales', e);
             }
         },
         'ecommerce.invoicing.OrderBilled' : function(s,e) {
             var metadata = JSON.parse(e.metadataRaw);
             var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.metadata.correlationId) {
+            if (body.payload && !metadata.content.correlationId) {
                 linkTo('sales', e);
             }
         },
         'ecommerce.invoicing.OrderBillingFailed' : function(s,e) {
             var metadata = JSON.parse(e.metadataRaw);
             var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.metadata.correlationId) {
+            if (body.payload && !metadata.content.correlationId) {
                 linkTo('sales', e);
             }
         }
