2fea42d766039ed59436202635dc569858320d21
pkaczor <newion@o2.pl>
2015-11-20 12:22:18 +0100

    make log level configurable at runtime

--- project/CommonDockerSettingsPlugin.scala
+++ project/CommonDockerSettingsPlugin.scala
@@ -5,12 +5,15 @@ import com.typesafe.sbt.packager.docker.{Cmd, DockerKeys}
 object CommonDockerSettingsPlugin extends AutoPlugin with DockerKeys {
   override def trigger = allRequirements
   override def requires = com.typesafe.sbt.packager.docker.DockerPlugin
+  def appLogLevel = sys.props.getOrElse("ECOMMERCE_LOG_LEVEL", default = "DEBUG")
+
   override lazy val projectSettings = Seq(
       dockerBaseImage := "java:8",
       dockerCommands ++= Seq(
         Cmd("MAINTAINER", "Pawel Kaczor <newion@o2.pl>"),
         Cmd("ENV", "ES_HOST=127.0.0.1"),
-        Cmd("ENV", "ES_PASSWORD=changeit")
+        Cmd("ENV", "ES_PASSWORD=changeit"),
+        Cmd("ENV", s"ECOMMERCE_LOG_LEVEL=$appLogLevel")
       )
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,11 +1,3 @@
-logLevel := Level.Info
-
-resolvers ++= Seq("Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/")
-
 addSbtPlugin("com.typesafe.sbt" % "sbt-multi-jvm" % "0.3.11")
 
-addSbtPlugin("com.orrsella" % "sbt-stats" % "1.0.5")
-
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.3")
-
-
