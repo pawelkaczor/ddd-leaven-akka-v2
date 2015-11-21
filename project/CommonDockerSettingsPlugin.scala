d6e77cfcd65daa118cd53a2014ef7c869d017c54
pkaczor <newion@o2.pl>
2015-11-21 17:56:21 +0100

    switch to minimal Docker image based on Alpine Linux

--- project/CommonDockerSettingsPlugin.scala
+++ project/CommonDockerSettingsPlugin.scala
@@ -8,12 +8,9 @@ object CommonDockerSettingsPlugin extends AutoPlugin with DockerKeys {
   def appLogLevel = sys.props.getOrElse("ECOMMERCE_LOG_LEVEL", default = "DEBUG")
 
   override lazy val projectSettings = Seq(
-      dockerBaseImage := "java:8",
+      dockerBaseImage := "newion/alpine",
       dockerCommands ++= Seq(
-        Cmd("MAINTAINER", "Pawel Kaczor <newion@o2.pl>"),
-        Cmd("ENV", "ES_HOST=127.0.0.1"),
-        Cmd("ENV", "ES_PASSWORD=changeit"),
-        Cmd("ENV", s"ECOMMERCE_LOG_LEVEL=$appLogLevel")
+        Cmd("ENV", s"ES_HOST=127.0.0.1 ES_PASSWORD=changeit ECOMMERCE_LOG_LEVEL=$appLogLevel")
       )
   )
 }
