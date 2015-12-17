22bbe6e79cd7efa3e7dac75d02c45d4027f421a1
pkaczor <newion@o2.pl>
2015-12-17 14:48:28 +0100

    clean up Eventstore projections

--- invoicing/write-back/src/main/resources/projections/invoicing-process.js
+++ invoicing/write-back/src/main/resources/projections/invoicing-process.js
@@ -1,24 +1,12 @@
 fromStreams(['$ce-Reservation', '$ce-Invoice']).
     when({
         'ecommerce.sales.ReservationConfirmed' : function(s,e) {
-            var metadata = JSON.parse(e.metadataRaw);
-            var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.content.correlationId) {
-                linkTo('invoicing', e);
-            }
+            linkTo('invoicing', e);
         },
         'ecommerce.invoicing.OrderBilled' : function(s,e) {
-            var metadata = JSON.parse(e.metadataRaw);
-            var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.content.correlationId) {
-                linkTo('invoicing', e);
-            }
+            linkTo('invoicing', e);
         },
         'ecommerce.invoicing.OrderBillingFailed' : function(s,e) {
-            var metadata = JSON.parse(e.metadataRaw);
-            var body = JSON.parse(e.bodyRaw);
-            if (body.payload && !metadata.content.correlationId) {
-                linkTo('invoicing', e);
-            }
+            linkTo('invoicing', e);
         }
     });
